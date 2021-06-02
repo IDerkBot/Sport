52812b3d04995830f92c994f2b1ccf31fc2be48c
IDerkBot <ivanderk2003@gmail.com>
2021-06-02 16:57:56 +0300

    fixed add new Equipment

--- AdminEditPages/EquipmentEditPage.xaml.cs
+++ AdminEditPages/EquipmentEditPage.xaml.cs
@@ -19,32 +19,39 @@ namespace Sport.AdminEditPages
         { Manager.MainFrame.Navigate(new AdminPages.EquipmentsPage()); }
         void Add_Click(object sender, RoutedEventArgs e)
         {
-            StringBuilder errors = new StringBuilder();
-            if (errors.Length > 0)
+            if(Name.Text != null && Price.Text != null && Storage.Text != null && Name.Text != "" && Price.Text != "" && Storage.Text != "")
             {
-                MessageBox.Show(errors.ToString());
-                return;
+                StringBuilder errors = new StringBuilder();
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                if (_currentEquipment.Id == 0) dbSportEntities.GetContext().Equipments.Add(_currentEquipment);
+                try
+                {
+                    dbSportEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                    Manager.MainFrame.Navigate(new AdminPages.EquipmentsPage());
+                }
+                catch (Exception ex)
+                { MessageBox.Show(ex.Message.ToString()); }
             }
-            if (_currentEquipment.Id == 0) dbSportEntities.GetContext().Equipments.Add(_currentEquipment);
-            try
-            {
-                dbSportEntities.GetContext().SaveChanges();
-                MessageBox.Show("Информация сохранена!");
-                Manager.MainFrame.Navigate(new AdminPages.EquipmentsPage());
-            }
-            catch (Exception ex)
-            { MessageBox.Show(ex.Message.ToString()); }
+            else MessageBox.Show("Введите данные во все поля");
         }
         void Storage_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (int.TryParse(Storage.Text, out int count))
+            if(Name.Text != null && dbSportEntities.GetContext().Equipments.Where(x => x.Name == Name.Text).ToList().Count != 0)
             {
-                
-                var _storage = dbSportEntities.GetContext().Equipments.Where(x => x.Name == Name.Text).Select(x => x.Storage).Single();
-                _storage = count;
-                Storage.Text = _storage.ToString();
+                if (int.TryParse(Storage.Text, out int count))
+                {
+
+                    var _storage = dbSportEntities.GetContext().Equipments.Where(x => x.Name == Name.Text).Select(x => x.Storage).Single();
+                    _storage = count;
+                    Storage.Text = _storage.ToString();
+                }
+                else { MessageBox.Show($"Введите только цифры!"); Storage.Text = "0"; }
             }
-            else { MessageBox.Show($"Введите только цифры!"); Storage.Text = "0"; }
         }
     }
 }
